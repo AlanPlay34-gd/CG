03f27c10888d720e7b90d2a54d67264da6ab6651
AlanPlay34-gd <lugininalan@gmail.com>
2025-12-06 16:52:18 +0300

    I make tranaspenet cube

--- Lab_03/Include/our_gl.h
+++ Lab_03/Include/our_gl.h
@@ -12,7 +12,8 @@ void viewport(int x, int y, int w, int h);
 void projection(float coeff=0.f);
 void lookat(Vec3f eye, Vec3f center, Vec3f up);
 
-struct IShader {
+struct IShader{
+    float alpha = 0.0f;
     virtual ~IShader();
     virtual Vec4f vertex(int iface, int nthvert) = 0;
     virtual bool fragment(Vec3f bar, TGAColor &color) = 0;
